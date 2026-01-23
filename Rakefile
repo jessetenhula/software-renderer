d713da5aa0de94ee3fc1aee075153e9482810ec0
jesse <jesse.tenhula@gmail.com>
2026-01-23 15:06:46 +0200

    minor Rakefile project structure change

--- Rakefile
+++ Rakefile
@@ -2,6 +2,6 @@ task :build do
   sh 'gcc src/*.c -o bin/renderer -I include -std=c99'
 end
 
-task :run => :build do
-  sh './bin/renderer.exe out.tga'
+task :run, [:out_file] do |task, args|
+  sh "./bin/renderer #{args[:out_file]}"
 end
--- src/main.c
+++ src/main.c
@@ -3,21 +3,18 @@
 
 #include "tgaimage.h"
 
-//void print_tga_header(TGAHeader header)
-//{
-//	printf("w %d, h %d\n", header.width, header.height);
-//	printf("x_origin %d, y_origin %d\n", header.x_origin, header.y_origin);
-//	printf("image type %d\n", header.image_type);
-//}
-
 int main(int argc, char *argv[])
 {
-	FILE *in = fopen("floor_diffuse.tga", "rb");
+	FILE *in = fopen("image/floor_diffuse.tga", "rb");
+	FILE *out = fopen("image/floor_diffuse_out.tga", "wb");
+
+	if (in == NULL) {
+		printf("Couldn't open input file\n");
+		return -1;
+	}
 
 	TGAHeader header;
 	Pixel *data = ReadTGAImageFromFile(in, &header);
-
-	FILE *out = fopen("floor_diffuse_out.tga", "wb");
 
 	WriteTGAImageToFile(out, header, data); 
 
